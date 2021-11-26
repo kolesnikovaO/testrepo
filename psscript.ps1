d918823682265e8556f9abd95eb05a0ae0100516
kolesnikovaO <32612936+kolesnikovaO@users.noreply.github.com>
2021-11-26 12:00:12 +0200

    Update psscript.ps1

--- psscript.ps1
+++ psscript.ps1
@@ -1,3 +1,3 @@
 Write-Host "Hello World" | Out-File Outputfile1.txt -Append
 Write-Host "Hello World2" | Out-File Outputfile1.txt -Append
-Write-Host "Hello World2" | Out-File Outputfile1.txt -Append
+Write-Host "Hello World2 hello 33333" | Out-File Outputfile1.txt -Append
