4e9e51d18d24caa8a3006479712f977e00c5ea9a
kolesnikovaO <32612936+kolesnikovaO@users.noreply.github.com>
2021-11-26 13:46:16 +0200

    Olyatest 2

--- psscript.ps1
+++ psscript.ps1
@@ -1,4 +1,4 @@
 Write-Host "Hello World" | Out-File Outputfile1.txt -Append
 Write-Host "Hello World2" | dfbbcfn Out-File Outputfile1.txt -Append
 Write-Host "Hello World2 99999999999 hello 33333" | Out-File Outputfile1.txt -Append
-Write-Host "Hello World2 hello Olyatest" | Out-File Outputfile1.txt -Append
+Write-Host "Hello World2 hello Olyatest 2" | Out-File Outputfile1.txt -Append
